ad25dd87fadb57e02d4bf982e5049c652e1f846a
duboffik <36671836+duboffik@users.noreply.github.com>
2024-06-30 14:50:00 +0400

    Update outputs.tf

--- task04/outputs.tf
+++ task04/outputs.tf
@@ -1,1 +1,34 @@
 
+/* output "ssh-key_data" {
+  value = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
+} 
+
+output "rg_name" {
+  value = azurerm_resource_group.my_RG.name
+}
+
+output "VNET_name" {
+  value = azurerm_virtual_network.my_VNET.name
+}
+
+output "VNET_address" {
+  value = azurerm_virtual_network.my_VNET.address_space
+}
+
+output "SNET_name" {
+  value = azurerm_subnet.my_SNET.name
+}
+
+output "SNET_address" {
+  value = azurerm_subnet.my_SNET.address_prefixes
+}*/
+
+output "PublicIP_address" {
+  value = azurerm_linux_virtual_machine.my_vm.public_ip_address
+}
+
+#output "PublicIP_dns" {
+#  value = azurerm_public_ip.my_public_ip.fqdn
+#}
+
+
