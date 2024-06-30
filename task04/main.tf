b11f62cb1275f25d19c69e4289622bdebdd2e004
duboffik <36671836+duboffik@users.noreply.github.com>
2024-06-30 13:59:07 +0400

    Update main.tf

--- task04/main.tf
+++ task04/main.tf
@@ -13,13 +13,13 @@ provider "azurerm" {
 
 # Create Resource group
 resource "azurerm_resource_group" "my_RG" {
-  name     = var.rg_name
-  location = var.rg_location
+  name     = ${var.rg_name}
+  location = ${var.rg_location}
 }
 
 # Create vNet
 resource "azurerm_virtual_network" "my_VNET" {
-  name                = var.vnet_name
+  name                = ${var.vnet_name}
   resource_group_name = azurerm_resource_group.my_RG.name
   location            = azurerm_resource_group.my_RG.location
   address_space       = ["${var.vnet_address}"]
@@ -27,7 +27,7 @@ resource "azurerm_virtual_network" "my_VNET" {
 
 # Create Subnet
 resource "azurerm_subnet" "my_SNET" {
-  name                 = var.snet_name
+  name                 = ${var.snet_name}
   virtual_network_name = azurerm_virtual_network.my_VNET.name
   resource_group_name  = azurerm_resource_group.my_RG.name
   address_prefixes     = ["${var.snet_prefixes}"]
@@ -35,21 +35,21 @@ resource "azurerm_subnet" "my_SNET" {
 
 # Create Public IP
 resource "azurerm_public_ip" "my_public_ip" {
-  name                = var.my_PublicIP_name
+  name                = ${var.my_PublicIP_name}
   resource_group_name = azurerm_resource_group.my_RG.name
   location            = azurerm_resource_group.my_RG.location
   allocation_method   = "Dynamic"
-  domain_name_label   = var.domain_name_label
+  domain_name_label   = ${var.domain_name_label}
 }
 
 resource "azurerm_network_security_group" "my_nsg" {
-  name                = var.my_NetworkSecurityGroup_name
+  name                = ${var.my_NetworkSecurityGroup_name}
   resource_group_name = azurerm_resource_group.my_RG.name
   location            = azurerm_resource_group.my_RG.location
 }
 
 resource "azurerm_network_security_rule" "my_rule1" {
-  name                       = var.secrule_name1
+  name                       = ${var.secrule_name1}
   priority                   = 1001
   direction                  = "Inbound"
   access                     = "Allow"
@@ -63,7 +63,7 @@ resource "azurerm_network_security_rule" "my_rule1" {
 }
 
 resource "azurerm_network_security_rule" "my_rule2" {
-  name                       = var.secrule_name2
+  name                       = ${var.secrule_name2}
   priority                   = 1002
   direction                  = "Inbound"
   access                     = "Allow"
@@ -78,12 +78,12 @@ resource "azurerm_network_security_rule" "my_rule2" {
 
 # Create network interface
 resource "azurerm_network_interface" "my_nic" {
-  name                = var.my_nic_name
+  name                = ${var.my_nic_name}
   resource_group_name = azurerm_resource_group.my_RG.name
   location            = azurerm_resource_group.my_RG.location
 
   ip_configuration {
-    name                          = var.my_configuration_name
+    name                          = ${var.my_configuration_name}
     subnet_id                     = azurerm_subnet.my_SNET.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.my_public_ip.id
@@ -98,29 +98,29 @@ resource "azurerm_network_interface_security_group_association" "nic_association
 
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "my_vm" {
-  name                            = var.computer_name
+  name                            = ${var.computer_name}
   location                        = azurerm_resource_group.my_RG.location
   resource_group_name             = azurerm_resource_group.my_RG.name
   network_interface_ids           = [azurerm_network_interface.my_nic.id]
-  size                            = var.vm_size
+  size                            = ${var.vm_size}
   disable_password_authentication = false
 
   os_disk {
-    name                 = var.my_disk_name
+    name                 = ${var.my_disk_name}
     caching              = "ReadWrite"
-    storage_account_type = var.storage_account_type
+    storage_account_type = ${var.storage_account_type}
   }
 
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = var.image_SKU
+    sku       = ${var.image_SKU}
     version   = "latest"
   }
 
-  computer_name  = var.computer_name
-  admin_username = var.admin_username
-  admin_password = var.admin_password
+  computer_name  = ${var.computer_name}
+  admin_username = ${var.admin_username}
+  admin_password = ${var.admin_password}
 
   # Remote-exec provisioner (NGINX configuration)
   provisioner "remote-exec" {
@@ -130,15 +130,15 @@ resource "azurerm_linux_virtual_machine" "my_vm" {
     ]
     connection {
       type     = "ssh"
-      user     = var.admin_username
-      password = var.admin_password
+      user     = ${var.admin_username}
+      password = ${var.admin_password}
       host     = azurerm_public_ip.my_public_ip.fqdn
     }
   }
 }
 
 resource "azurerm_virtual_machine_extension" "my_extension" {
-  name                 = var.ext_name
+  name                 = ${var.ext_name}
   virtual_machine_id   = azurerm_linux_virtual_machine.my_vm.id
   publisher            = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
