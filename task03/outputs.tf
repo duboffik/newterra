17146b88987461a1d7cac5572d73ffa6aba76b88
duboffik <36671836+duboffik@users.noreply.github.com>
2024-07-10 18:39:24 +0400

    Update outputs.tf

--- task03/outputs.tf
+++ task03/outputs.tf
@@ -2,19 +2,19 @@
   value = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
 } */
 
-output "rg_name" {
+output "rg_ID" {
   description = "test_description_1"
-  value = azurerm_resource_group.my_RG.name
+  value = azurerm_resource_group.my_RG.id
 }
 
-output "sa_name" {
+output "sa_blob_endpoint" {
   description = "test_description_2"
-  value = azurerm_storage_account.my_SA.name
+  value = azurerm_storage_account.my_SA.primary_blob_endpoint
 }
 
-output "VNET_name" {
+output "VNET_ID" {
   description = "test_description_3"
-  value = azurerm_virtual_network.my_VNET.name
+  value = azurerm_virtual_network.my_VNET.id
 }
 
 output "VNET_address" {
