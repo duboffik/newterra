dc26f442d6e88aaa961891491b90282a628e7cc5
duboffik <36671836+duboffik@users.noreply.github.com>
2024-07-10 15:00:44 +0400

    Update outputs.tf

--- task03/outputs.tf
+++ task03/outputs.tf
@@ -3,14 +3,17 @@
 } */
 
 output "rg_name" {
+  description = test_description_1
   value = azurerm_resource_group.my_RG.name
 }
 
 output "sa_name" {
+  description = test_description_2
   value = azurerm_storage_account.my_SA.name
 }
 
 output "VNET_name" {
+  description = test_description_3
   value = azurerm_virtual_network.my_VNET.name
 }
 
@@ -19,18 +22,22 @@ output "VNET_address" {
 }
 
 output "SNET1_name" {
+  description = test_description_4
   value = azurerm_subnet.my_SNET1.name
 }
 
 output "SNET1_address" {
+  description = test_description_5
   value = azurerm_subnet.my_SNET1.address_prefixes
 }
 
 output "SNET2_name" {
+  description = test_description_6
   value = azurerm_subnet.my_SNET2.name
 }
 
 output "SNET2_address" {
+  description = test_description_7
   value = azurerm_subnet.my_SNET2.address_prefixes
 }
 
