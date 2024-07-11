91309a2dd3e8f2be015a2e52e74cc4843f9d0e3f
duboffik <36671836+duboffik@users.noreply.github.com>
2024-07-11 16:52:56 +0400

    Update main.tf

--- task03/main.tf
+++ task03/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
 
   }
-/*   backend "azurerm" {
+  /*   backend "azurerm" {
       resource_group_name  = "module2tfstate002"
       storage_account_name = "module2tfstate002"
       container_name       = "module2tfstate002"
@@ -21,13 +21,13 @@ provider "azurerm" {
 
 # Create Resource group
 resource "azurerm_resource_group" "my_RG" {
-  name     = "${var.rg_name}"
-  location = "${var.rg_location}"
+  name     = var.rg_name
+  location = var.rg_location
 }
 
 # Create vNet
 resource "azurerm_virtual_network" "my_VNET" {
-  name                = "${var.vnet_name}"
+  name                = var.vnet_name
   resource_group_name = azurerm_resource_group.my_RG.name
   location            = azurerm_resource_group.my_RG.location
   address_space       = ["${var.vnet_address}"]
@@ -35,25 +35,25 @@ resource "azurerm_virtual_network" "my_VNET" {
 
 # Create Subnet
 resource "azurerm_subnet" "my_SNET1" {
-  name                 = "${var.snet_name1}"
+  name                 = var.snet_name1
   virtual_network_name = azurerm_virtual_network.my_VNET.name
-  resource_group_name = azurerm_resource_group.my_RG.name
+  resource_group_name  = azurerm_resource_group.my_RG.name
   address_prefixes     = ["${var.snet1_prefixes}"]
 }
 
 # Create Subnet
 resource "azurerm_subnet" "my_SNET2" {
-  name                 = "${var.snet_name2}"
+  name                 = var.snet_name2
   virtual_network_name = azurerm_virtual_network.my_VNET.name
-  resource_group_name = azurerm_resource_group.my_RG.name
+  resource_group_name  = azurerm_resource_group.my_RG.name
   address_prefixes     = ["${var.snet2_prefixes}"]
 }
 
 # Create Storage account
 resource "azurerm_storage_account" "my_SA" {
-  name                     = "${var.sa_name}"
-  resource_group_name = azurerm_resource_group.my_RG.name
-  location            = azurerm_resource_group.my_RG.location
+  name                     = var.sa_name
+  resource_group_name      = azurerm_resource_group.my_RG.name
+  location                 = azurerm_resource_group.my_RG.location
   account_kind             = "BlockBlobStorage"
   account_tier             = "Premium"
   account_replication_type = "LRS"
