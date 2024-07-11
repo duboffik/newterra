a7fccd618b00717104e5c65d271dcc850c5e0a42
duboffik <36671836+duboffik@users.noreply.github.com>
2024-07-11 16:52:36 +0400

    Update outputs.tf

--- task03/outputs.tf
+++ task03/outputs.tf
@@ -1,14 +1,14 @@
 output "rg_ID" {
   description = "test_description_1"
-  value = azurerm_resource_group.my_RG.id
+  value       = azurerm_resource_group.my_RG.id
 }
 
 output "sa_blob_endpoint" {
   description = "test_description_2"
-  value = azurerm_storage_account.my_SA.primary_blob_endpoint
+  value       = azurerm_storage_account.my_SA.primary_blob_endpoint
 }
 
 output "VNET_ID" {
   description = "test_description_3"
-  value = azurerm_virtual_network.my_VNET.id
+  value       = azurerm_virtual_network.my_VNET.id
 }
