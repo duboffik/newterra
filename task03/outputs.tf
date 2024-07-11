b0234e411365227071c6e85ef8b095d4a6849746
duboffik <36671836+duboffik@users.noreply.github.com>
2024-07-11 16:39:20 +0400

    Update outputs.tf

--- task03/outputs.tf
+++ task03/outputs.tf
@@ -1,7 +1,3 @@
-/* output "ssh-key_data" {
-  value = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
-} */
-
 output "rg_ID" {
   description = "test_description_1"
   value = azurerm_resource_group.my_RG.id
@@ -16,28 +12,3 @@ output "VNET_ID" {
   description = "test_description_3"
   value = azurerm_virtual_network.my_VNET.id
 }
-
-output "VNET_address" {
-  value = azurerm_virtual_network.my_VNET.address_space
-}
-
-output "SNET1_name" {
-  description = "test_description_4"
-  value = azurerm_subnet.my_SNET1.name
-}
-
-output "SNET1_address" {
-  description = "test_description_5"
-  value = azurerm_subnet.my_SNET1.address_prefixes
-}
-
-output "SNET2_name" {
-  description = "test_description_6"
-  value = azurerm_subnet.my_SNET2.name
-}
-
-output "SNET2_address" {
-  description = "ID of project VPC"
-  value = azurerm_subnet.my_SNET2.address_prefixes
-}
-
