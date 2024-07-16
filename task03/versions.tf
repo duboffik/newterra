75984525dafbeb8c3a3af7a9c0af78bbfec0df76
duboffik <36671836+duboffik@users.noreply.github.com>
2024-07-16 13:31:34 +0400

    Create versions.tf

---
+++ task03/versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.110.0, < 4.0.0"
+    }
+
+  }
+  /*   backend "azurerm" {
+      resource_group_name  = "module2tfstate002"
+      storage_account_name = "module2tfstate002"
+      container_name       = "module2tfstate002"
+      key                  = "terraform.tfstate"
+  } */
+  required_version = ">= 1.5.7"
+}
