89f4b0dcae9d815187b98e2ebdf3445befdc3212
duboffik <36671836+duboffik@users.noreply.github.com>
2024-07-16 13:31:17 +0400

    Update main.tf

--- task03/main.tf
+++ task03/main.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.110.0, < 4.0.0"
-    }
-
-  }
-  /*   backend "azurerm" {
-      resource_group_name  = "module2tfstate002"
-      storage_account_name = "module2tfstate002"
-      container_name       = "module2tfstate002"
-      key                  = "terraform.tfstate"
-  } */
-  required_version = ">= 1.5.7"
-}
-
 provider "azurerm" {
   features {}
 }
