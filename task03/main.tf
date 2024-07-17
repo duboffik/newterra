926b64d893721f6ee0ca19460e0bb213bad8a536
duboffik <36671836+duboffik@users.noreply.github.com>
2024-07-17 17:02:33 +0400

    Update main.tf

--- task03/main.tf
+++ task03/main.tf
@@ -4,8 +4,7 @@ provider "azurerm" {
 
 # Create Resource group
 resource "azurerm_resource_group" "my_RG" {
-  name = test
-  #var.rg_name
+  name = var.rg_name
   location = var.rg_location
   tags = {
     Creator = "student_email"
