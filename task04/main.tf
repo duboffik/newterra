cb0d1150055f217f00121f2b4d5e81281255c721
Dzmitry Dubovik <dzmitry_dubovik@epam.com>
2024-08-05 12:51:09 +0400

    11

--- task04/main.tf
+++ task04/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "StorageAccountResourceGroup"
-    storage_account_name = "myterraformstorage"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
-
 provider "azurerm" {
   features {}
 }
