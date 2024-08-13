06950749c7b0005fcbb857ba05a5adaea29b75ee
Dzmitry Dubovik <dzmitry_dubovik@epam.com>
2024-08-14 00:06:53 +0400

    14

--- task05/outputs.tf
+++ task05/outputs.tf
@@ -1,4 +1,4 @@
-output "azurerm_traffic_manager_profile" {
-  description = "The FQDN of the created Profile."
+output "traffic_manager_fqdn" {
+  description = "The FQDN of the created Profile"
   value       = module.traffic_manager.traffic_manager_fqdn
 }
